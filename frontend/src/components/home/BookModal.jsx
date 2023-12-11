56c2a5664ce6f3ae60a3a503bacb646c5fbc86cc
sreenijaearanki <sreenijaearanki@gmail.com>
2023-12-10 21:22:51 -0500

    changes

--- frontend/src/components/home/BookModal.jsx
+++ frontend/src/components/home/BookModal.jsx
@@ -16,9 +16,11 @@ const BookModal = ({ book, onClose }) => {
           className='absolute right-6 top-6 text-3xl text-red-600 cursor-pointer'
           onClick={onClose}
         />
+        {/*
         <h2 className='w-fit px-4 py-1 bg-red-300 rounded-lg'>
           {book.publishYear}
         </h2>
+  */}
         <h4 className='my-2 text-gray-500'>{book._id}</h4>
         <div className='flex justify-start items-center gap-x-2'>
           <PiBookOpenTextLight className='text-red-300 text-2xl' />
